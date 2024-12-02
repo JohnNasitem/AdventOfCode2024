eb6e0f664180ddb3f14962731b952c041cff4475
JohnNasitem <jnasitem1@nait.ca>
2024-12-01 19:20:17 -0700

    finished functionality

--- Day1_HistorianHysteria/HistorianHysteria.cs
+++ Day1_HistorianHysteria/HistorianHysteria.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Day1_HistorianHysteria
 {
     public partial class HistorianHysteria : Form
     {
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+        Stopwatch stopwatch = new Stopwatch();
+
         public HistorianHysteria()
         {
             InitializeComponent();
@@ -20,6 +26,36 @@ namespace Day1_HistorianHysteria
         private void UI_DragDropSection_Lbl_DragDrop(object sender, DragEventArgs e)
         {
             string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+            long totalDistance = 0;
+            stopwatch.Restart();
+
+            try
+            {
+                stopwatch.Start();
+
+                foreach (string line in File.ReadAllLines(fname))
+                {
+                    leftList.Add(Convert.ToInt32(line.Split(' ')[0]));
+                    rightList.Add(Convert.ToInt32(line.Split(' ')[3]));
+                }
+
+                leftList.Sort();
+                rightList.Sort();
+
+                for (int i = 0; i < leftList.Count; i++)
+                    totalDistance += Math.Abs(leftList[i] - rightList[i]);
+
+                stopwatch.Stop();
+
+                UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedMilliseconds} ms";
+                UI_Output_Tbx.Text = totalDistance.ToString();
+
+            }
+            catch
+            {
+                MessageBox.Show("Failed to read file.");
+                return;
+            }
         }
 
         private void UI_DragDropSection_Lbl_DragEnter(object sender, DragEventArgs e)
