c42098891d46c6f479005703a657f787e202c60d
JohnNasitem <jnasitem1@nait,ca>
2024-12-04 13:44:15 -0700

    finished part 2

--- Day4_CeresSearch/CeresSearch.Designer.cs
+++ Day4_CeresSearch/CeresSearch.Designer.cs
@@ -31,7 +31,10 @@
             this.UI_DragDrop_Lbl = new DottedLabel.DottedLabel();
             this.UI_XmaxCountLabel_Lbl = new System.Windows.Forms.Label();
             this.UI_XmasCount_Tbx = new System.Windows.Forms.TextBox();
-            this.UI_TimeTaken_Lbl = new System.Windows.Forms.Label();
+            this.UI_TimeTakenPart1_Lbl = new System.Windows.Forms.Label();
+            this.UI_XmasCountPart2_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_XmasCountPart2Label_Lbl = new System.Windows.Forms.Label();
+            this.UI_TimeTakenPart2_Lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UI_DragDrop_Lbl
@@ -43,7 +46,7 @@
             this.UI_DragDrop_Lbl.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold);
             this.UI_DragDrop_Lbl.Location = new System.Drawing.Point(12, 9);
             this.UI_DragDrop_Lbl.Name = "UI_DragDrop_Lbl";
-            this.UI_DragDrop_Lbl.Size = new System.Drawing.Size(1324, 686);
+            this.UI_DragDrop_Lbl.Size = new System.Drawing.Size(1923, 686);
             this.UI_DragDrop_Lbl.TabIndex = 0;
             this.UI_DragDrop_Lbl.Text = "Drag and Drop Input Here";
             this.UI_DragDrop_Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -71,23 +74,58 @@
             this.UI_XmasCount_Tbx.Size = new System.Drawing.Size(243, 42);
             this.UI_XmasCount_Tbx.TabIndex = 2;
             // 
-            // UI_TimeTaken_Lbl
+            // UI_TimeTakenPart1_Lbl
             // 
-            this.UI_TimeTaken_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.UI_TimeTaken_Lbl.AutoSize = true;
-            this.UI_TimeTaken_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UI_TimeTaken_Lbl.Location = new System.Drawing.Point(460, 702);
-            this.UI_TimeTaken_Lbl.Name = "UI_TimeTaken_Lbl";
-            this.UI_TimeTaken_Lbl.Size = new System.Drawing.Size(308, 34);
-            this.UI_TimeTaken_Lbl.TabIndex = 3;
-            this.UI_TimeTaken_Lbl.Text = "Time Taken: Unknown";
+            this.UI_TimeTakenPart1_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_TimeTakenPart1_Lbl.AutoSize = true;
+            this.UI_TimeTakenPart1_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_TimeTakenPart1_Lbl.Location = new System.Drawing.Point(908, 702);
+            this.UI_TimeTakenPart1_Lbl.Name = "UI_TimeTakenPart1_Lbl";
+            this.UI_TimeTakenPart1_Lbl.Size = new System.Drawing.Size(405, 34);
+            this.UI_TimeTakenPart1_Lbl.TabIndex = 3;
+            this.UI_TimeTakenPart1_Lbl.Text = "Time Taken (Part1): Unknown";
+            // 
+            // UI_XmasCountPart2_Tbx
+            // 
+            this.UI_XmasCountPart2_Tbx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_XmasCountPart2_Tbx.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_XmasCountPart2_Tbx.Location = new System.Drawing.Point(659, 698);
+            this.UI_XmasCountPart2_Tbx.Name = "UI_XmasCountPart2_Tbx";
+            this.UI_XmasCountPart2_Tbx.ReadOnly = true;
+            this.UI_XmasCountPart2_Tbx.Size = new System.Drawing.Size(243, 42);
+            this.UI_XmasCountPart2_Tbx.TabIndex = 5;
+            // 
+            // UI_XmasCountPart2Label_Lbl
+            // 
+            this.UI_XmasCountPart2Label_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_XmasCountPart2Label_Lbl.AutoSize = true;
+            this.UI_XmasCountPart2Label_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_XmasCountPart2Label_Lbl.Location = new System.Drawing.Point(460, 702);
+            this.UI_XmasCountPart2Label_Lbl.Name = "UI_XmasCountPart2Label_Lbl";
+            this.UI_XmasCountPart2Label_Lbl.Size = new System.Drawing.Size(205, 34);
+            this.UI_XmasCountPart2Label_Lbl.TabIndex = 4;
+            this.UI_XmasCountPart2Label_Lbl.Text = "X-MAS Count:";
+            // 
+            // UI_TimeTakenPart2_Lbl
+            // 
+            this.UI_TimeTakenPart2_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_TimeTakenPart2_Lbl.AutoSize = true;
+            this.UI_TimeTakenPart2_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_TimeTakenPart2_Lbl.Location = new System.Drawing.Point(1341, 702);
+            this.UI_TimeTakenPart2_Lbl.Name = "UI_TimeTakenPart2_Lbl";
+            this.UI_TimeTakenPart2_Lbl.Size = new System.Drawing.Size(405, 34);
+            this.UI_TimeTakenPart2_Lbl.TabIndex = 6;
+            this.UI_TimeTakenPart2_Lbl.Text = "Time Taken (Part2): Unknown";
             // 
             // CeresSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1348, 753);
-            this.Controls.Add(this.UI_TimeTaken_Lbl);
+            this.ClientSize = new System.Drawing.Size(1947, 753);
+            this.Controls.Add(this.UI_TimeTakenPart2_Lbl);
+            this.Controls.Add(this.UI_XmasCountPart2_Tbx);
+            this.Controls.Add(this.UI_XmasCountPart2Label_Lbl);
+            this.Controls.Add(this.UI_TimeTakenPart1_Lbl);
             this.Controls.Add(this.UI_XmasCount_Tbx);
             this.Controls.Add(this.UI_XmaxCountLabel_Lbl);
             this.Controls.Add(this.UI_DragDrop_Lbl);
@@ -103,7 +141,10 @@
         private DottedLabel.DottedLabel UI_DragDrop_Lbl;
         private System.Windows.Forms.Label UI_XmaxCountLabel_Lbl;
         private System.Windows.Forms.TextBox UI_XmasCount_Tbx;
-        private System.Windows.Forms.Label UI_TimeTaken_Lbl;
+        private System.Windows.Forms.Label UI_TimeTakenPart1_Lbl;
+        private System.Windows.Forms.TextBox UI_XmasCountPart2_Tbx;
+        private System.Windows.Forms.Label UI_XmasCountPart2Label_Lbl;
+        private System.Windows.Forms.Label UI_TimeTakenPart2_Lbl;
     }
 }
 
--- Day4_CeresSearch/CeresSearch.cs
+++ Day4_CeresSearch/CeresSearch.cs
@@ -10,6 +10,8 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Xml.Linq;
+using System.IO.Ports;
 
 namespace Day4_CeresSearch
 {
@@ -26,27 +28,31 @@ namespace Day4_CeresSearch
         private void UI_DragDrop_Lbl_DragDrop(object sender, DragEventArgs e)
         {
             string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();        //Get dropped file name
-            Stopwatch stopwatch = new Stopwatch();                                          //Measure how long it takes to get the output
-            int xmasCount = 0;
+            Stopwatch stopwatch1 = new Stopwatch(), stopwatch2 = new Stopwatch();                                          //Measure how long it takes to get the output
+
+            int xmasCount1 = 0, xmasCount2 = 0;
 
             try
             {
-                stopwatch.Start();
-
                 chars = File.ReadAllLines(fname).ToArray();
 
                 wordSearchWidth = chars.Length;
                 wordSearchHeight = chars.Length;
 
-                for (int y = 0; y < wordSearchHeight; y++)
-                    for (int x = 0; x < wordSearchWidth; x++)
-                        xmasCount += FoundXmasCount(x, y);
+                stopwatch1.Start();
+                xmasCount1 = Part1();
+                stopwatch1.Stop();
 
-                stopwatch.Stop();
+                stopwatch2.Start();
+                xmasCount2 = Part2();
+                stopwatch2.Stop();
 
                 //Output values
-                UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
-                UI_XmasCount_Tbx.Text = xmasCount.ToString();
+                UI_TimeTakenPart1_Lbl.Text = $"Time taken: {stopwatch1.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
+                UI_TimeTakenPart2_Lbl.Text = $"Time taken: {stopwatch2.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
+                UI_XmasCount_Tbx.Text = xmasCount1.ToString();
+                UI_XmasCountPart2_Tbx.Text = xmasCount2.ToString();
+
             }
             catch (Exception ex)
             {
@@ -69,59 +75,109 @@ namespace Day4_CeresSearch
                 e.Effect = DragDropEffects.None;
         }
 
-        private int FoundXmasCount(int x, int y)
+        private int FoundXmasCountPart1(int x, int y)
         {
             int foundXmasCount = 0;
 
             //Check left
             if (x >= 3 && 
-                DoesPositionsContainXmas(new Point(x, y), new Point(x - 1, y), new Point(x - 2, y), new Point(x - 3, y)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x - 1, y), new Point(x - 2, y), new Point(x - 3, y)))
                 foundXmasCount++;
 
             //Check right
             if (x <= wordSearchWidth - 4 && 
-                DoesPositionsContainXmas(new Point(x, y), new Point(x + 1, y), new Point(x + 2, y), new Point(x + 3, y)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x + 1, y), new Point(x + 2, y), new Point(x + 3, y)))
                 foundXmasCount++;
 
             //Check up
             if (y >= 3 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x, y - 1), new Point(x, y - 2), new Point(x, y - 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x, y - 1), new Point(x, y - 2), new Point(x, y - 3)))
                 foundXmasCount++;
 
             //Check down
             if (y <= wordSearchHeight - 4 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x, y + 1), new Point(x, y + 2), new Point(x, y + 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x, y + 1), new Point(x, y + 2), new Point(x, y + 3)))
                 foundXmasCount++;
 
             //Check top right
             if (y >= 3 && x <= wordSearchWidth - 4 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x + 1, y - 1), new Point(x + 2, y - 2), new Point(x + 3, y - 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x + 1, y - 1), new Point(x + 2, y - 2), new Point(x + 3, y - 3)))
                 foundXmasCount++;
 
             //Check top left
             if (y >= 3 && x >= 3 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x - 1, y - 1), new Point(x - 2, y - 2), new Point(x - 3, y - 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x - 1, y - 1), new Point(x - 2, y - 2), new Point(x - 3, y - 3)))
                 foundXmasCount++;
 
             //Check bottom right
             if (y <= wordSearchHeight - 4 && x <= wordSearchWidth - 4 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x + 1, y + 1), new Point(x + 2, y + 2), new Point(x + 3, y + 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x + 1, y + 1), new Point(x + 2, y + 2), new Point(x + 3, y + 3)))
                 foundXmasCount++;
 
             //Check bottom left
             if (y <= wordSearchHeight - 4 && x >= 3 &&
-                DoesPositionsContainXmas(new Point(x, y), new Point(x - 1, y + 1), new Point(x - 2, y + 2), new Point(x - 3, y + 3)))
+                DoesPositionsContainXmasPart1(new Point(x, y), new Point(x - 1, y + 1), new Point(x - 2, y + 2), new Point(x - 3, y + 3)))
                 foundXmasCount++;
 
             return foundXmasCount;
         }
 
-        private bool DoesPositionsContainXmas(Point pos1, Point pos2, Point pos3, Point pos4)
+        private bool DoesPositionsContainXmasPart1(Point pos1, Point pos2, Point pos3, Point pos4)
         {
             if (chars[pos1.X][pos1.Y] == 'X' && chars[pos2.X][pos2.Y] == 'M' && chars[pos3.X][pos3.Y] == 'A' && chars[pos4.X][pos4.Y] == 'S')
                 return true;
 
             return false;
+        }
+
+        private int Part1()
+        {
+            int xmasCount = 0;
+
+            for (int y = 0; y < wordSearchHeight; y++)
+                for (int x = 0; x < wordSearchWidth; x++)
+                    xmasCount += FoundXmasCountPart1(x, y);
+
+            return xmasCount;
+        }
+
+        private int Part2()
+        {
+            int xmasCount = 0;
+
+            for (int y = 0; y < wordSearchHeight; y++)
+                for (int x = 0; x < wordSearchWidth; x++)
+                    if (FoundXmasCountPart2(x, y))
+                        xmasCount++;
+
+            return xmasCount;
+        }
+
+        private bool FoundXmasCountPart2(int x, int y)
+        {
+            char[] cornerChars = null;
+
+            if (x == 0 || y == 0 || x == wordSearchWidth - 1 || y == wordSearchHeight - 1)
+                return false;
+
+            if (chars[x][y] != 'A')
+                return false;
+
+            cornerChars = new char[] {
+                chars[x - 1][y - 1],    //Top left
+                chars[x - 1][y + 1],    //Bottom left
+                chars[x + 1][y + 1],    //Bottom right
+                chars[x + 1][y - 1]     //Top right
+            };
+
+            if (cornerChars.Contains('A') || cornerChars.Contains('X'))
+                return false;
+
+            Console.WriteLine($"0: {cornerChars[0]} != 2: {cornerChars[2]}, 1: {cornerChars[1]} != 3: {cornerChars[3]}");
+            if (cornerChars[0] != cornerChars[2] && cornerChars[1] != cornerChars[3])
+                return true;
+            else
+                return false;
         }
     }
 }
