7af0e7906416b0a30a23dd11b29131f33b546fe7
JohnNasitem <jnasitem1@nait.ca>
2024-12-03 21:00:00 -0700

    finished drag drop base

--- Day2_MullItOver/MullItOver.cs
+++ Day2_MullItOver/MullItOver.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Day2_MullItOver
 {
@@ -19,12 +20,20 @@ namespace Day2_MullItOver
 
         private void UI_DragDropSection_Lbl_DragDrop(object sender, DragEventArgs e)
         {
-
+            string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
         }
 
+        /// <summary>
+        /// Allow drop effect
+        /// </summary>
+        /// <param name="sender">Object that called the code</param>
+        /// <param name="e">Event args</param>
         private void UI_DragDropSection_Lbl_DragEnter(object sender, DragEventArgs e)
         {
-
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
         }
     }
 }
