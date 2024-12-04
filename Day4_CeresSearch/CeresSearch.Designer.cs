8f72ce6813c576ecec22c1bb743eb04d46204493
JohnNasitem <jnasitem1@nait,ca>
2024-12-04 12:46:08 -0700

    finished ui

--- Day4_CeresSearch/CeresSearch.Designer.cs
+++ Day4_CeresSearch/CeresSearch.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.UI_DragDrop_Lbl = new DottedLabel.DottedLabel();
+            this.UI_XmaxCountLabel_Lbl = new System.Windows.Forms.Label();
+            this.UI_XmasCount_Lbl = new System.Windows.Forms.TextBox();
+            this.UI_TimeTaken_Lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // UI_DragDrop_Lbl
+            // 
+            this.UI_DragDrop_Lbl.AllowDrop = true;
+            this.UI_DragDrop_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_DragDrop_Lbl.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold);
+            this.UI_DragDrop_Lbl.Location = new System.Drawing.Point(12, 9);
+            this.UI_DragDrop_Lbl.Name = "UI_DragDrop_Lbl";
+            this.UI_DragDrop_Lbl.Size = new System.Drawing.Size(1324, 686);
+            this.UI_DragDrop_Lbl.TabIndex = 0;
+            this.UI_DragDrop_Lbl.Text = "Drag and Drop Input Here";
+            this.UI_DragDrop_Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI_XmaxCountLabel_Lbl
+            // 
+            this.UI_XmaxCountLabel_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_XmaxCountLabel_Lbl.AutoSize = true;
+            this.UI_XmaxCountLabel_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_XmaxCountLabel_Lbl.Location = new System.Drawing.Point(12, 702);
+            this.UI_XmaxCountLabel_Lbl.Name = "UI_XmaxCountLabel_Lbl";
+            this.UI_XmaxCountLabel_Lbl.Size = new System.Drawing.Size(193, 34);
+            this.UI_XmaxCountLabel_Lbl.TabIndex = 1;
+            this.UI_XmaxCountLabel_Lbl.Text = "XMAS Count:";
+            // 
+            // UI_XmasCount_Lbl
+            // 
+            this.UI_XmasCount_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_XmasCount_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_XmasCount_Lbl.Location = new System.Drawing.Point(211, 698);
+            this.UI_XmasCount_Lbl.Name = "UI_XmasCount_Lbl";
+            this.UI_XmasCount_Lbl.ReadOnly = true;
+            this.UI_XmasCount_Lbl.Size = new System.Drawing.Size(243, 42);
+            this.UI_XmasCount_Lbl.TabIndex = 2;
+            // 
+            // UI_TimeTaken_Lbl
+            // 
+            this.UI_TimeTaken_Lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_TimeTaken_Lbl.AutoSize = true;
+            this.UI_TimeTaken_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_TimeTaken_Lbl.Location = new System.Drawing.Point(460, 702);
+            this.UI_TimeTaken_Lbl.Name = "UI_TimeTaken_Lbl";
+            this.UI_TimeTaken_Lbl.Size = new System.Drawing.Size(308, 34);
+            this.UI_TimeTaken_Lbl.TabIndex = 3;
+            this.UI_TimeTaken_Lbl.Text = "Time Taken: Unknown";
             // 
             // CeresSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1348, 753);
+            this.Controls.Add(this.UI_TimeTaken_Lbl);
+            this.Controls.Add(this.UI_XmasCount_Lbl);
+            this.Controls.Add(this.UI_XmaxCountLabel_Lbl);
+            this.Controls.Add(this.UI_DragDrop_Lbl);
             this.Name = "CeresSearch";
             this.Text = "Ceres Search";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DottedLabel.DottedLabel UI_DragDrop_Lbl;
+        private System.Windows.Forms.Label UI_XmaxCountLabel_Lbl;
+        private System.Windows.Forms.TextBox UI_XmasCount_Lbl;
+        private System.Windows.Forms.Label UI_TimeTaken_Lbl;
     }
 }
 
