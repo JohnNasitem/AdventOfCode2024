8f035937d9cb0ea6fb3fa892b8b02f95cdc19b1a
JohnNasitem <jnasitem1@nait.ca>
2024-12-03 20:54:24 -0700

    renamed form

--- Day2_MullItOver/MullItOver.cs
+++ Day2_MullItOver/MullItOver.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Day2_MullItOver
 {
-    public partial class Form1 : Form
+    public partial class MullItOver : Form
     {
-        public Form1()
+        public MullItOver()
         {
             InitializeComponent();
         }
--- Day2_MullItOver/Program.cs
+++ Day2_MullItOver/Program.cs
@@ -16,7 +16,7 @@ namespace Day2_MullItOver
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MullItOver());
         }
     }
 }
