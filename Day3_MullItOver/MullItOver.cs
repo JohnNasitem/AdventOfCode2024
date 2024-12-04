66aed5622696d2590552f806123f403313aa6187
JohnNasitem <jnasitem1@nait.ca>
2024-12-03 21:25:29 -0700

    Commented code

--- Day3_MullItOver/MullItOver.cs
+++ Day3_MullItOver/MullItOver.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//***********************************************************************************
+//Program: MullItOver.cs          
+//Description: Extract working code from program (input)
+//Date: Dec. 3/2024
+//Author: John Nasitem
+//Advent of Code 2024 Challenge - Day 3
+//https://adventofcode.com/2024/day/3
+//https://adventofcode.com/2024/day/3#part2
+//***********************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,32 +30,50 @@ namespace Day2_MullItOver
             InitializeComponent();
         }
 
+
+
+        /// <summary>
+        /// Program (input) contains mul(), do(), and don't() commands
+        /// Extract the intact commands and get the output
+        /// </summary>
+        /// <param name="sender">Object that called the code</param>
+        /// <param name="e">Event args</param>
         private void UI_DragDropSection_Lbl_DragDrop(object sender, DragEventArgs e)
         {
-            string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
-            Stopwatch stopwatch = new Stopwatch();
-            int mulTotal = 0;
+            string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();        //Get dropped file name
+            Stopwatch stopwatch = new Stopwatch();                                          //Measure how long it takes to get the output
+            int mulTotal = 0;                                                               //Output
+            bool doMul = true;                                                              //Was last command do() or don't()
 
             try
             {
                 stopwatch.Start();
 
-                foreach (Match match in Regex.Matches(File.ReadAllText(fname), @"mul\(\d{1,3},\d{1,3}\)"))
+                //Go over program and extract the intact mul(), do(), and don't commands
+                //Add the mul() output to the total if the more recent command was do()
+                foreach (Match match in Regex.Matches(File.ReadAllText(fname), @"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"))
                 {
-                    Console.WriteLine(match.Value);
-                    int[] numbers = new int[2];
-                    int i = 0;
+                    int[] numbers = new int[2];         //2 numbers in the mul() command
+                    int i = 0;                          //Increment var
 
-                    foreach (Match num in Regex.Matches(match.Value, @"\d{1,3}"))
-                    {
-                        numbers[i++] = int.Parse(num.Value);
-                    }
+                    //Update the most recent command
+                    if (match.Value == "do()")
+                        doMul = true;
+                    else if (match.Value == "don't()")
+                        doMul = false;
+                    //Get the numbers inside of mul()
+                    else
+                        foreach (Match num in Regex.Matches(match.Value, @"\d{1,3}"))
+                            numbers[i++] = int.Parse(num.Value);
 
-                    mulTotal += numbers[0] * numbers[1];
+                    //If last command is do() then add the mul() output to the total
+                    if (doMul)
+                        mulTotal += numbers[0] * numbers[1];
                 }
 
                 stopwatch.Stop();
 
+                //Output values
                 UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
                 UI_MulTotal_Tbx.Text = mulTotal.ToString();
             }
@@ -55,6 +83,8 @@ namespace Day2_MullItOver
                 return;
             }
         }
+
+
 
         /// <summary>
         /// Allow drop effect
