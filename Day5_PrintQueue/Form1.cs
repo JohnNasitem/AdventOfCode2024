1fedabb8532eac98747465fbc649a95c042e6328
JohnNasitem <jnasitem1@nait,ca>
2024-12-05 12:48:53 -0700

    Finished part 1

--- Day5_PrintQueue/Form1.cs
+++ Day5_PrintQueue/Form1.cs
@@ -9,11 +9,14 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Day5_PrintQueue
 {
     public partial class Form1 : Form
     {
+        Dictionary<int, List<int>> pageOrderingRules = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,21 +27,53 @@ namespace Day5_PrintQueue
             string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();        //Get dropped file name
             Stopwatch stopwatch = new Stopwatch();           //Measure how long it takes to get the output
             int middleSum = 0;
+            pageOrderingRules = new Dictionary<int, List<int>>();
+            List<List<int>> pagesToUpdate = new List<List<int>>();
+            bool isDataOrderingRules = true;
 
-            try
-            {
-                stopwatch.Start();
-                stopwatch.Stop();
+            //try
+            //{
+            //    
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Failed to read file.");
+            //    return;
+            //}
 
-                //Output values
-                UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
-                UI_MiddleSum_Tbx.Text = middleSum.ToString();
-            }
-            catch (Exception ex)
+            stopwatch.Start();
+
+            //Extract the data
+            foreach (string line in File.ReadAllLines(fname))
             {
-                MessageBox.Show("Failed to read file.");
-                return;
+                if (line == "")
+                {
+                    isDataOrderingRules = false;
+                    continue;
+                }
+
+                if (isDataOrderingRules)
+                {
+                    int key = int.Parse(line.Split('|')[0]);
+                    if (pageOrderingRules.ContainsKey(key))
+                        pageOrderingRules[key].Add(int.Parse(line.Split('|')[1]));
+                    else
+                        pageOrderingRules.Add(key, new List<int>() { int.Parse(line.Split('|')[1]) });
+                }
+                else
+                    pagesToUpdate.Add(line.Split(',').Select(int.Parse).ToList());
             }
+
+            foreach (List<int> page in pagesToUpdate)
+                if (IsUpdateInRightOrder(page))
+                    middleSum += page[(int)Math.Floor(page.Count / 2.0)];
+
+
+            stopwatch.Stop();
+
+            //Output values
+            UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
+            UI_MiddleSum_Tbx.Text = middleSum.ToString();
         }
 
 
@@ -53,6 +88,15 @@ namespace Day5_PrintQueue
                 e.Effect = DragDropEffects.Copy;
             else
                 e.Effect = DragDropEffects.None;
+        }
+
+        private bool IsUpdateInRightOrder(List<int> update)
+        {
+            for (int i = update.Count - 1; i > 0; i--)
+                if (pageOrderingRules[update[i]].Intersect(update.GetRange(0, i)).ToList().Count > 0)
+                    return false;
+
+            return true;
         }
     }
 }
