a724b29bcc63edc6bcc71b21c5c48d976b43bfd3
JohnNasitem <jnasitem1@nait,ca>
2024-12-05 12:08:16 -0700

    Finished program base

--- Day5_PrintQueue/Form1.cs
+++ Day5_PrintQueue/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,42 @@ namespace Day5_PrintQueue
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void UI_DragDrop_Lbl_DragDrop(object sender, DragEventArgs e)
+        {
+            string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();        //Get dropped file name
+            Stopwatch stopwatch = new Stopwatch();           //Measure how long it takes to get the output
+            int middleSum = 0;
+
+            try
+            {
+                stopwatch.Start();
+                stopwatch.Stop();
+
+                //Output values
+                UI_TimeTaken_Lbl.Text = $"Time taken: {stopwatch.ElapsedTicks * (1.0 / Stopwatch.Frequency) * 1000} ms";
+                UI_MiddleSum_Tbx.Text = middleSum.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to read file.");
+                return;
+            }
+        }
+
+
+        /// <summary>
+        /// Allow drop effect
+        /// </summary>
+        /// <param name="sender">Object that called the code</param>
+        /// <param name="e">Event args</param>
+        private void UI_DragDrop_Lbl_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
         }
     }
 }
