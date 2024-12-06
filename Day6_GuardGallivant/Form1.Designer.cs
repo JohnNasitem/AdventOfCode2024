e88b7b6e91efeb026a74e7a8ed9f93e47ca899ff
JohnNasitem <jnasitem1@nait,ca>
2024-12-06 13:30:31 -0700

    Finished part 1 ui

--- Day6_GuardGallivant/Form1.Designer.cs
+++ Day6_GuardGallivant/Form1.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.UI_DragDrop_Lbl = new DottedLabel.DottedLabel();
+            this.UI_VisitedPositionsCountLabel_Lbl = new System.Windows.Forms.Label();
+            this.UI_VisitedPositionsCount_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_TimeTaken_Lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // UI_DragDrop_Lbl
+            // 
+            this.UI_DragDrop_Lbl.AllowDrop = true;
+            this.UI_DragDrop_Lbl.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold);
+            this.UI_DragDrop_Lbl.Location = new System.Drawing.Point(12, 9);
+            this.UI_DragDrop_Lbl.Name = "UI_DragDrop_Lbl";
+            this.UI_DragDrop_Lbl.Size = new System.Drawing.Size(1718, 678);
+            this.UI_DragDrop_Lbl.TabIndex = 0;
+            this.UI_DragDrop_Lbl.Text = "Drag and Drop Input Here";
+            this.UI_DragDrop_Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI_VisitedPositionsCountLabel_Lbl
+            // 
+            this.UI_VisitedPositionsCountLabel_Lbl.AutoSize = true;
+            this.UI_VisitedPositionsCountLabel_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_VisitedPositionsCountLabel_Lbl.Location = new System.Drawing.Point(12, 694);
+            this.UI_VisitedPositionsCountLabel_Lbl.Name = "UI_VisitedPositionsCountLabel_Lbl";
+            this.UI_VisitedPositionsCountLabel_Lbl.Size = new System.Drawing.Size(318, 34);
+            this.UI_VisitedPositionsCountLabel_Lbl.TabIndex = 1;
+            this.UI_VisitedPositionsCountLabel_Lbl.Text = "Visited Positions Count";
+            // 
+            // UI_VisitedPositionsCount_Tbx
+            // 
+            this.UI_VisitedPositionsCount_Tbx.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_VisitedPositionsCount_Tbx.Location = new System.Drawing.Point(332, 690);
+            this.UI_VisitedPositionsCount_Tbx.Name = "UI_VisitedPositionsCount_Tbx";
+            this.UI_VisitedPositionsCount_Tbx.ReadOnly = true;
+            this.UI_VisitedPositionsCount_Tbx.Size = new System.Drawing.Size(284, 42);
+            this.UI_VisitedPositionsCount_Tbx.TabIndex = 2;
+            // 
+            // UI_TimeTaken_Lbl
+            // 
+            this.UI_TimeTaken_Lbl.AutoSize = true;
+            this.UI_TimeTaken_Lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_TimeTaken_Lbl.Location = new System.Drawing.Point(622, 694);
+            this.UI_TimeTaken_Lbl.Name = "UI_TimeTaken_Lbl";
+            this.UI_TimeTaken_Lbl.Size = new System.Drawing.Size(308, 34);
+            this.UI_TimeTaken_Lbl.TabIndex = 3;
+            this.UI_TimeTaken_Lbl.Text = "Time Taken: Unknown";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1742, 744);
+            this.Controls.Add(this.UI_TimeTaken_Lbl);
+            this.Controls.Add(this.UI_VisitedPositionsCount_Tbx);
+            this.Controls.Add(this.UI_VisitedPositionsCountLabel_Lbl);
+            this.Controls.Add(this.UI_DragDrop_Lbl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DottedLabel.DottedLabel UI_DragDrop_Lbl;
+        private System.Windows.Forms.Label UI_VisitedPositionsCountLabel_Lbl;
+        private System.Windows.Forms.TextBox UI_VisitedPositionsCount_Tbx;
+        private System.Windows.Forms.Label UI_TimeTaken_Lbl;
     }
 }
 
